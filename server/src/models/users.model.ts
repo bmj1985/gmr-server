ad13ee9ac5921b2667297045155ac841ea59d0be
Brandon Johnson <bmj1985@gmail.com>
2019-09-30 17:38:27 -0600

    server boilerplate

---
+++ server/src/models/users.model.ts
@@ -0,0 +1,24 @@
+// users-model.js - A mongoose model
+//
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+import { Application } from '../declarations';
+
+export default function (app: Application) {
+  const mongooseClient = app.get('mongooseClient');
+  const users = new mongooseClient.Schema({
+  
+    email: {type: String, unique: true, lowercase: true},
+    password: { type: String },
+  
+  
+    auth0Id: { type: String },
+  
+    googleId: { type: String },
+  
+  }, {
+    timestamps: true
+  });
+
+  return mongooseClient.model('users', users);
+}
