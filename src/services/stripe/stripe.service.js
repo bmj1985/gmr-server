ce81260d6cb1c999cad2187c6caa2df69813e465
Brandon Johnson <bmj1985@gmail.com>
2020-04-08 18:03:59 -0600

    figuring out which Stripe APIs to use, will likely need to change again

--- src/services/stripe/stripe.service.js
+++ src/services/stripe/stripe.service.js
@@ -1,7 +1,13 @@
 // Initializes the `stripe` service on path `/stripe`
 const { Stripe } = require("./stripe.class")
 const hooks = require("./stripe.hooks")
-const { Charge, Product, Order } = require("feathers-stripe")
+const {
+  Charge,
+  Product,
+  Order,
+  Sku,
+  PaymentIntent,
+} = require("feathers-stripe")
 
 module.exports = function(app) {
   const paginate = app.get("paginate")
@@ -15,13 +21,19 @@ module.exports = function(app) {
   app.use("/stripe/charges", new Charge({ secretKey: apiKey }))
   app.use("stripe/products", new Product({ secretKey: apiKey }))
   app.use("stripe/orders", new Order({ secretKey: apiKey }))
+  app.use("stripe/skus", new Sku({ secretKey: apiKey }))
+  app.use("stripe/payment-intents", new PaymentIntent({ secretKey: apiKey }))
 
   // Get our initialized service so that we can register hooks
   const chargeService = app.service("stripe/charges")
   const productsService = app.service("stripe/products")
   const orderService = app.service("stripe/orders")
+  const skusService = app.service("stripe/skus")
+  const paymentIntentService = app.service("stripe/payment-intents")
 
   chargeService.hooks(hooks)
   productsService.hooks(hooks)
   orderService.hooks(hooks)
+  skusService.hooks(hooks)
+  paymentIntentService.hooks(hooks)
 }
